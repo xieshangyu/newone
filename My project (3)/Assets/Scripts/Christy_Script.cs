0ad74876c67e045a2590ae5dbd62eb3510de9cd9
Christy-Lin <cl5501@nyu.edu>
2023-02-17 17:27:19 -0500

    Scene & Script

---
+++ My project (3)/Assets/Scripts/Christy_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Christy_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Christy's Script");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
