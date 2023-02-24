aed26209accd7ee00a344e87b2f59cf8248c3127
xieshangyu <76835319+xieshangyu@users.noreply.github.com>
2023-02-23 23:25:23 -0500

    add bullet prop
    bullet prop

--- My project (3)/Assets/Scripts/player_level4.cs
+++ My project (3)/Assets/Scripts/player_level4.cs
@@ -13,6 +13,7 @@ public class player_level4 : MonoBehaviour
     public Transform spawnPoint;
     public GameObject bulletPrefab;
 
+    public int gun = 1;
     Rigidbody2D rigid_body_2D;
     AudioSource audio_source;
     GameManager game_manager;
@@ -54,12 +55,50 @@ public class player_level4 : MonoBehaviour
             if (Shoot_sound == null) {
                 Debug.LogError("Shoot_sound has not been assigned.");
             }
-            audio_source.PlayOneShot(Shoot_sound);
-            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(2,bulletSpeed));
+
+            if(gun==1)
+            {
+                audio_source.PlayOneShot(Shoot_sound);
+                GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(2,bulletSpeed));
+            }
+
+            else if (gun == 2)
+            {
+                
+                audio_source.PlayOneShot(Shoot_sound);
+                // Spawn two bullets, one on each side of the player
+                GameObject newBullet1 = Instantiate(bulletPrefab, transform.position + new Vector3(-0.2f, 0, 0), Quaternion.identity);
+                newBullet1.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, bulletSpeed));
+                  // Add force to the bullets to make them move
+                GameObject newBullet2 = Instantiate(bulletPrefab, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
+                newBullet2.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, bulletSpeed));
+            }
+
+            else if (gun == 3) {
+                audio_source.PlayOneShot(Shoot_sound);
+
+                // Spawn three bullets, one on each side and one in the middle of the player
+                GameObject newBullet1 = Instantiate(bulletPrefab, transform.position + new Vector3(-0.4f, 0f, 0f), Quaternion.identity);
+                GameObject newBullet2 = Instantiate(bulletPrefab, transform.position + new Vector3(0f, 0f, 0f), Quaternion.identity);
+                GameObject newBullet3 = Instantiate(bulletPrefab, transform.position + new Vector3(0.4f, 0f, 0f), Quaternion.identity);
+
+                // Add force to the bullets to make them move
+                newBullet1.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, bulletSpeed));
+                newBullet2.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, bulletSpeed));
+                newBullet3.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, bulletSpeed));
+            }
+
         }
     }
     
+
+    public void Add_Bullet(int bullet) {
+        gun += bullet;
+        Debug.Log("Bullet: " + gun);
+
+        // livesUI.text = "LIVES: " + lives;
+    }
     // private void OnTriggerEnter2D(Collider2D other)
     // {
     //     if (other.gameObject.tag == "enemy")
